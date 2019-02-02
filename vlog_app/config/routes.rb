5b400814c4a78b2fa108a50a85e2da8bfc8b0625
elord-dna <814592344@qq.com>
2019-02-02 21:17:54 +0800

    add some url

--- vlog_app/config/routes.rb
+++ vlog_app/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  #root 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
+  get '/signup', to: 'users#signup'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

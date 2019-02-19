c78cb80d7fc8ffe7eb18079f0a011b272f7421dc
elord-dna <814592344@qq.com>
2019-02-19 17:57:55 +0800

    half sign

--- vlog_app/config/routes.rb
+++ vlog_app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#signup'
+  post '/signup', to: 'users#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

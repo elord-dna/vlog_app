e49684cab5c35936ddf5c48c82cc9b063160e30d
elord-dna <814592344@qq.com>
2019-03-10 20:35:07 +0800

    modify micropost

--- vlog_app/app/controllers/application_controller.rb
+++ vlog_app/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    private
+
+    # 确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        # store_location
+        # redirect_to login_url
+        format.html { redirect_to :login, notice: '您还没有登陆，请登陆'}
+      end
+    end
 end
--- vlog_app/app/controllers/microposts_controller.rb
+++ vlog_app/app/controllers/microposts_controller.rb
@@ -1,2 +1,19 @@
 class MicropostsController < ApplicationController
+    before_action :logged_in_user, only: [:create, :destroy]
+
+    def create
+        @micropost = current_user.microposts.build(micropost_params)
+        if @micropost.save
+            format.html { redirect_to root_url, notice: 'Micropost created!'}
+        else
+            render 'static_pages/home'
+        end
+    end
+
+    
+    private
+
+        def micropost_params
+            params.require(:micropost).permit(:content)
+        end
 end
